0ad5a204cc9b1bbc3ceceab2f72ad6a45a689d32
Jimmy Tang <blay.tenshi@gmail.com>
2023-04-25 22:01:01 +1000

    Converted to use Vite instead of Vue-CLI

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,12 @@ const routeProperties = ["path", "name", "component"];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: routes.map((route) => pick(route, routeProperties))
+  routes: routes.map((route) => {
+    const routes = pick(route, routeProperties);
+
+    console.log(routes);
+    return routes;
+  }),
 });
 
 export default router;
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,3 @@
-import HomePage from "../views/HomePage";
-
 /*
   path - url to the view in router,
   name - name of the component, not important, just for uniqueness when rendering all the HomePage.vue
@@ -12,54 +10,54 @@ export default [
     path: "/",
     name: "HomePage",
     displayName: "Home Page",
-    component: HomePage
+    component: () => import("../views/HomePage.vue")
   },
   {
     path: "/maska",
     name: "MaskaPage",
     displayName: "Maska Page",
-    component: () => import("../views/MaskaPage")
+    component: () => import("../views/MaskaPage.vue")
   },
   {
     path: "/vue-3-vmodel-native-elements",
     name: "Vue3VModelNativeElements",
     displayName: "Vue 3 V-Model Native Inputs",
-    component: () => import("../views/Vue3VModelNativeElements")
+    component: () => import("../views/Vue3VModelNativeElements.vue")
   },
   {
     path: "/vue-3-vmodel-custom-elements",
     name: "Vue3VModelCustomElements",
     displayName: "Vue 3 V-Model Custom Elements",
-    component: () => import("../views/Vue3VModelCustomElements")
+    component: () => import("../views/Vue3VModelCustomElements.vue")
   },
   {
     path: "/composition-api-basics",
     name: "CompositionApiBasics",
     displayName: "Composition API Basics",
-    component: () => import("../views/CompositionApiBasics")
+    component: () => import("../views/CompositionApiBasics.vue")
   },
   {
     path: "/vbind-props-to-child",
     name: "VbindPropsToChild",
     displayName: "v-bind props to Child Components",
-    component: () => import("../views/VbindPropsToChild")
+    component: () => import("../views/VbindPropsToChild.vue")
   },
   {
     path: "/watchers",
     name: "WatchersExample",
     displayName: "Watchers",
-    component: () => import("../views/Watchers")
+    component: () => import("../views/Watchers.vue")
   },
   {
     path: "/rxjs",
     name: "RxJS",
     displayName: "RxJS Example",
-    component: () => import("../views/RxJS")
+    component: () => import("../views/RxJS.vue")
   },
   {
     path: "/svg-imports",
     name: "SvgImports",
     displayName: "SVG Imports",
-    component: () => import("../views/SvgImports")
+    component: () => import("../views/SvgImports.vue")
   }
 ];
