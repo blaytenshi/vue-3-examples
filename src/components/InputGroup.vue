008a179486f600941136e167f9c5e3a9732de400
Jimmy Tang <blay.tenshi@gmail.com>
2024-04-25 16:33:05 +0800

    updated the InputGroup component

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,27 +1,39 @@
 <script setup>
+// This is the Input Field Group
+// The label is optional and will not render if there's no label given
 import { func, string } from "vue-types";
 import { noop } from "lodash";
 
 defineProps({
   inputName: string().def("inputName"),
-  inputLabel: string().def("Input Name"),
+  inputLabel: string(),
   type: string().def("text"),
   errorMessage: string().def(""),
   inputValue: string().def(""),
   handleInputChange: func().def(noop()),
+  handleBlur: func().def(noop()),
+  placeholder: string().def("Placeholder"),
 });
 </script>
 
 <template>
   <div class="input-group-wrapper">
-    <label :for="inputName">{{ inputLabel }}</label>
+    <label
+      v-if="inputLabel"
+      :for="inputName"
+    >{{ inputLabel }}</label>
     <input
       :id="inputName"
+      :name="inputName"
       :type="type"
       :value="inputValue"
+      :placeholder="placeholder"
       @input="handleInputChange"
+      @blur="handleBlur"
     >
-    <div>{{ errorMessage }}</div>
+    <div class="input-group-error-message">
+      {{ errorMessage }}
+    </div>
   </div>
 </template>
 
@@ -29,5 +41,8 @@ defineProps({
 .input-group-wrapper {
   display: flex;
   flex-direction: column;
+}
+.input-group-error-message {
+  color: red;
 }
 </style>
--- src/views/basic-forms-validation/index.vue
+++ src/views/basic-forms-validation/index.vue
@@ -1,15 +1,55 @@
 <script setup>
 import InputGroup from "../../components/InputGroup.vue";
-import { reactive } from "vue";
+import { computed, reactive } from "vue";
 
 const state = reactive({
-  inputValue: "",
-  inputValueErrorMsg: "",
+  form: {
+    name: {
+      inputValue: "",
+      inputValueErrorMsg: "",
+      placeholder: "Your name",
+    },
+    email: {
+      inputValue: "",
+      inputValueErrorMsg: "",
+      placeholder: "Your email",
+    },
+  },
 });
 
 const handleInputChange = (event) => {
-  console.log(event.target.value);
-  state.inputValue = event.target.value;
+  const { name, value } = event.target;
+
+  state.form[name].inputValue = value;
+  if (state.form[name].inputValueErrorMsg) {
+    state.form[name].inputValueErrorMsg = "";
+  }
+};
+
+const handleBlur = (event) => {
+  const { name } = event.target;
+  state.form[name].inputValueErrorMsg = validateInput(state.form[name].inputValue, name);
+};
+
+// This can be improved on so we're not validating by the field name each time.
+// replace with yup or zod
+const validateInput = (inputValue, inputName) => {
+  if (inputName === "email" && !inputValue.includes("@")) {
+    return "Must be an email!";
+  }
+
+  if (inputName === "name" && inputValue.length <= 1) {
+    return "Input must be longer than 1 character!";
+  }
+};
+
+const isSubmitDisabled = computed(() => Object.keys(state.form)
+  .map(inputName => state.form[inputName].inputValueErrorMsg)
+  .some(errorMsg => errorMsg.length > 0), // check if there's actual content in the error message
+);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
 };
 </script>
 
@@ -20,10 +60,29 @@ const handleInputChange = (event) => {
     input-name="name"
     input-label="Name"
     type="text"
-    :input-value="state.inputValue"
+    :input-value="state.form.name.inputValue"
     :handle-input-change="handleInputChange"
-    :error-message="state.inputValueErrorMsg"
+    :handle-blur="handleBlur"
+    :error-message="state.form.name.inputValueErrorMsg"
+    :placeholder="state.form.name.placeholder"
   />
+  <InputGroup
+    input-name="email"
+    input-label="Email"
+    type="text"
+    :input-value="state.form.email.inputValue"
+    :handle-input-change="handleInputChange"
+    :handle-blur="handleBlur"
+    :error-message="state.form.email.inputValueErrorMsg"
+    :placeholder="state.form.email.placeholder"
+  />
+  <button
+    :disabled="isSubmitDisabled"
+    type="button"
+    @click="handleSubmit"
+  >
+    Submit
+  </button>
 </template>
 
 <style scoped>
