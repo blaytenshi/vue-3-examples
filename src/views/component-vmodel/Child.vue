1270ed1dd5b6f1aac7826bac0a2e6a8bf0bf8a57
Jimmy Tang <blay.tenshi@gmail.com>
2024-08-05 00:19:46 +0800

    Left out Custom V-Model Example

---
+++ src/views/component-vmodel/Child.vue
@@ -0,0 +1,18 @@
+<script setup>
+const model = defineModel();
+
+const update = () => {
+  model.value++;
+};
+</script>
+
+<template>
+  <div>Parent bound v-model is: {{ model }}</div>
+  <button @click="update">
+    Increment
+  </button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/component-vmodel/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import Child from "./Child.vue";
+</script>
+
+<template>
+  <h1>Component VModel with defineModel in Vue 3.4+</h1>
+  <Child v-model="countModel" />
+</template>
+
+<style scoped>
+
+</style>
