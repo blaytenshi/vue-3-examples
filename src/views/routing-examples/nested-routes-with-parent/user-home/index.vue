14d355e29f77e8db13446b243863376fcdc52820
Jimmy Tang <blay.tenshi@gmail.com>
2025-04-04 01:21:53 +0800

    Issue 55 - Fix Routing Bug

--- src/views/routing-examples/nested-routes-with-parent/user-home/index.vue
+++ src/views/routing-examples/nested-routes-with-parent/user-home/index.vue
@@ -1,27 +1,18 @@
 <script setup>
 import { useRoute } from "vue-router";
+import Container from "@/components/Container.vue";
+import { CONTAINER_DIRECTIONS } from "@/constants/options.js";
 
 const route = useRoute();
 
-const { userName } = route.params;
+const { username } = route.params;
 </script>
 
 <template>
-  <h1>User Home {{ userName }}</h1>
-  <p>This is the User's home page component. Each of the links below take you to yet another child route of '/nested-routes-with-parent/users'</p>
-  <p>Hover over the links to see the routes they will take you to.</p>
-  <ul>
-    <li>
-      <RouterLink :to="`${userName}/posts`">
-        Posts
-      </RouterLink>
-    </li>
-    <li>
-      <RouterLink :to="`${userName}/address`">
-        Address
-      </RouterLink>
-    </li>
-  </ul>
+  <Container :direction="CONTAINER_DIRECTIONS.COLUMN">
+    <p>This is the {{ username }}'s home page component. Each of the links below take you to yet another child route of '/nested-routes-with-parent/users'</p>
+    <p>Hover over the links to see the routes they will take you to.</p>
+  </Container>
 </template>
 
 <style scoped>
