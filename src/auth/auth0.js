b30acb778ca3f9ad06d5304f905fd17f308a313d
Jimmy Tang <blay.tenshi@gmail.com>
2025-05-16 01:26:54 +0800

    Added Auth0 with universal login example

---
+++ src/auth/auth0.js
@@ -0,0 +1,17 @@
+import { createAuth0 } from "@auth0/auth0-vue";
+
+let auth0;
+
+export function initAuth0() {
+  if (!auth0) {
+    auth0 = createAuth0({
+      domain: "dev-76xdkh7gv88mcbr5.us.auth0.com",
+      clientId: "iSyvYOcef2q8VpjwoUDtRIXVbIcshK2L",
+      authorizationParams: {
+        redirect_uri: "http://localhost:3030",
+      },
+    });
+  }
+
+  return auth0;
+}
