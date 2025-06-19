a306d0b44cde21336aac07436fc427b4c3cfc094
Jimmy Tang <blay.tenshi@gmail.com>
2025-06-19 23:49:38 +0800

    Add Updated Emits Example

---
+++ src/views/vue-fundamentals/emits/custom-text-input.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    default: "Input Text Label",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <label for="custom-input">{{ label }}</label>
+    <input
+      id="custom-input"
+      v-model="inputValue"
+      type="text"
+      @input="emitInput"
+    >
+  </div>
+</template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
