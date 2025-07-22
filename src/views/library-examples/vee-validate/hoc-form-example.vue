77f94c4b80e5bd249b2421ba37ac2dca9c87b1c5
Jimmy Tang <blay.tenshi@gmail.com>
2025-07-22 23:40:30 +0800

    Updated Vee-Validate HOC Form validation example

--- src/views/library-examples/vee-validate/hoc-form-example.vue
+++ src/views/library-examples/vee-validate/hoc-form-example.vue
@@ -2,6 +2,8 @@
 import { required } from "@vee-validate/rules";
 import { Form, Field, ErrorMessage, defineRule } from "vee-validate";
 import * as yup from "yup";
+import { CONTAINER_DIRECTIONS } from "@/constants/options.js";
+import Container from "@/components/Container.vue";
 
 defineRule("required", required);
 
@@ -39,45 +41,71 @@ function onProfileSubmit(values) {
 </script>
 
 <template>
-  <Form
-    :validation-schema="loginSchema"
-    @submit="onLoginSubmit"
-  >
-    <Field
-      name="email"
-      placeholder="Email"
-    />
-    <ErrorMessage name="email" />
-    <Field
-      name="name"
-      placeholder="Name"
-    />
-    <ErrorMessage name="name" />
-    <Field
-      name="password"
-      type="password"
-      placeholder="Password"
-    />
-    <ErrorMessage name="password" />
-    <button>Submit Login</button>
-  </Form>
-  <!-- Form that uses yup validation schema instead -->
-  <Form
-    :validation-schema="profileSchema"
-    @submit="onProfileSubmit"
-  >
-    <Field
-      name="firstName"
-      placeholder="First Name"
-    />
-    <ErrorMessage name="firstName" />
-    <Field
-      name="lastName"
-      placeholder="Last Name"
-    />
-    <ErrorMessage name="lastName" />
-    <button>Submit Profile</button>
-  </Form>
+  <Container :direction="CONTAINER_DIRECTIONS.COLUMN">
+    <h1>Vee Validate Higher Order Component (HOC) Form Validation Example</h1>
+    <p>
+      This page contains example of using Vee Validate's Higher Order Component (HOC) to validate forms by placing the
+      validation directly on the form itself instead of on invidividual fields.
+    </p>
+    <h2>Example One - Basic Validation using Form schema</h2>
+    <p>
+      When using the Form component from Vee Validate, the @submit event is triggered when the form is submitted and
+      the validation schema is applied to all fields within the form.
+    </p>
+    <p>
+      You can define the schema as an object with the names of each field as the key and the rule as a function to
+      check as the value that is passed into the validation function. The rule function, like validation rules for
+      fields must match the property name.
+    </p>
+    <Form
+      :validation-schema="loginSchema"
+      @submit="onLoginSubmit"
+    >
+      <Field
+        name="email"
+        placeholder="Email"
+      />
+      <ErrorMessage name="email" />
+      <Field
+        name="name"
+        placeholder="Name"
+      />
+      <ErrorMessage name="name" />
+      <Field
+        name="password"
+        type="password"
+        placeholder="Password"
+      />
+      <ErrorMessage name="password" />
+      <!--
+        By HTML defaults, a button within the <form> tag means it will automatically be a submit button (type="submit")
+        unless declared otherwise.
+      -->
+      <button type="submit">
+        Submit Login
+      </button>
+    </Form>
+    <h2>Example Two - Validating Form with Yup validation schema</h2>
+    <!-- Form that uses yup validation schema instead -->
+    <Form
+      :validation-schema="profileSchema"
+      @submit="onProfileSubmit"
+    >
+      <Field
+        name="firstName"
+        placeholder="First Name"
+      />
+      <ErrorMessage name="firstName" />
+      <Field
+        name="lastName"
+        placeholder="Last Name"
+      />
+      <ErrorMessage name="lastName" />
+      <button type="submit">
+        Submit Profile
+      </button>
+    </Form>
+  </Container>
 </template>
 
 <style scoped>
